fb167c5c0fe1c3df52725dbb3d27d50be30a1ac4
Zsolt Tóth <waysofdarkness@gmail.com>
2020-10-06 15:17:58 +0200

    Improved version created by Daniël Sonck

--- svg2dds.sh
+++ svg2dds.sh
@@ -1,38 +1,42 @@
-#!/bin/env sh
-NVENC(){
+#!/bin/sh
+
+die() {
+  echo "$@"
+  exit 1
+}
+
+NVENC() {
 	name="$1"
-	ddsfilename=`basename -s .png "$name"`".dds"
-    nvcompress -bc1 "$name" "$ddsfilename"
+    nvcompress -bc1 "$name.png" "$name.dds"
 	rm "$name"
 }
-S2TC(){
+
+S2TC() {
 	export S2TC_COLORDIST_MODE=SRGB_MIXED
 	export S2TC_RANDOM_COLORS=64
 	export S2TC_REFINE_COLORS=LOOP
 	export S2TC_DITHER_MODE=FLOYDSTEINBERG
 	name="$1"
-	tgafilename=`basename -s .png "$name"`".tga"
-	ddsfilename=`basename -s .png "$name"`".dds"
-	convert "$name" "$tgafilename"
-	s2tc_compress -i "$tgafilename" -o "$ddsfilename" -t DXT1
-	rm "$name"
-	rm "$tgafilename"
+	convert "$name.png" "$name.tga"
+	s2tc_compress -i "$name.tga" -o "$name.dds" -t DXT1
+	rm "$name.png"
+	rm "$name.tga"
 }
-if [ "$#" -lt 2 ]; then echo "Usage: $0 NVENC|S2TC <input>"; exit 1; fi
+
+[ $# -lt 2 ] && die "Usage: $(basename "$0") NVENC|S2TC <input.svg>"
+
 method="$1"
 shift
 filename="$1"
-pngfilename=`basename -s .svg "$filename"`".png"
-case $method in
-NVENC)
+shift
+base="$(basename -s .svg "$filename")"
+
 inkscape --export-type="png" "$filename"
-NVENC "$pngfilename"
-;;
-S2TC)
-inkscape --export-type="png" "$filename"
-S2TC "$pngfilename"
+case "$method" in
+NVENC|S2TC)
+	$method "$base"
 ;;
 *)
-echo 'Only NVENC or S2TC is supported'
+  die "Unsupported format: $method"
 ;;
 esac
