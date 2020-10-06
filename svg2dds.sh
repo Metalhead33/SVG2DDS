b67063723b0c573bed9749a2b81d2a1b950c1337
Zsolt Tóth <waysofdarkness@gmail.com>
2020-10-06 15:37:32 +0200

    Now it creates temporary PNG and TGA files

--- svg2dds.sh
+++ svg2dds.sh
@@ -6,9 +6,9 @@ die() {
 }
 
 NVENC() {
-	name="$1"
-    nvcompress -bc1 "$name.png" "$2"
-	rm "$name.png"
+	pngfile="$1"
+    nvcompress -bc1 "$pngfile" "$2"
+	rm "$pngfile"
 }
 
 S2TC() {
@@ -16,11 +16,12 @@ S2TC() {
 	export S2TC_RANDOM_COLORS=64
 	export S2TC_REFINE_COLORS=LOOP
 	export S2TC_DITHER_MODE=FLOYDSTEINBERG
-	name="$1"
-	convert "$name.png" "$name.tga"
-	s2tc_compress -i "$name.tga" -o "$2" -t DXT1
-	rm "$name.png"
-	rm "$name.tga"
+	pngfile="$1"
+	tgafile="$(mktemp --suffix=.tga)"
+	convert "$pngfile" "$tgafile"
+	s2tc_compress -i "$tgafile" -o "$2" -t DXT1
+	rm "$pngfile"
+	rm "$tgafile"
 }
 
 [ $# -lt 3 ] && die "Usage: $(basename "$0") NVENC|S2TC <input.svg> <output.dds>"
@@ -31,12 +32,12 @@ filename="$1"
 shift
 output="$1"
 shift
-base="$(basename -s .svg "$filename")"
+pngfile="$(mktemp --suffix=.png)"
 
-inkscape --export-type="png" "$filename"
+inkscape --export-type="png" --export-filename="$pngfile" "$filename"
 case "$method" in
 NVENC|S2TC)
-	$method "$base" "$output"
+	$method "$pngfile" "$output"
 ;;
 *)
   die "Unsupported format: $method"
