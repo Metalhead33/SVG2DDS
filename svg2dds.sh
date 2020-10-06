cb3773f4bff578a2c3c51d207978cf91ca895a8d
Zsolt Tóth <waysofdarkness@gmail.com>
2020-10-06 15:14:16 +0200

    Removed any remanining bashisms

--- svg2dds.sh
+++ svg2dds.sh
@@ -1,7 +1,8 @@
 #!/bin/env sh
 NVENC(){
 	name="$1"
-    nvcompress -bc1 "$name" "${name/.png/.dds}"
+	ddsfilename=`basename -s .png "$name"`".dds"
+    nvcompress -bc1 "$name" "$ddsfilename"
 	rm "$name"
 }
 S2TC(){
@@ -10,35 +11,28 @@ S2TC(){
 	export S2TC_REFINE_COLORS=LOOP
 	export S2TC_DITHER_MODE=FLOYDSTEINBERG
 	name="$1"
-	convert "$name" "${name/.png/.tga}"
-	s2tc_compress -i "${name/.png/.tga}" -o "${name/.png/.dds}" -t DXT1
+	tgafilename=`basename -s .png "$name"`".tga"
+	ddsfilename=`basename -s .png "$name"`".dds"
+	convert "$name" "$tgafilename"
+	s2tc_compress -i "$tgafilename" -o "$ddsfilename" -t DXT1
 	rm "$name"
-	rm "${name/.png/.tga}"
+	rm "$tgafilename"
 }
-if [ -z "$1" ]; then
-	echo 'You need to define compression method: NVENC or S2TC.'
-	echo '(Use NVENC only if you have an nVidia card!)'
-	exit 1
-fi
-if [ "$1" != "NVENC" ] && [ "$1" != "S2TC" ]; then
-	echo 'You need to define compression method: NVENC or S2TC.'
-	echo '(Use NVENC only if you have an nVidia card!)'
-	exit 1
-fi
-if [ -z "$2" ]; then
-	echo 'Please define an image as an input to convert.'
-	exit 1
-fi
-filename="$2"
-pngfilename="${filename/.svg/.png}"
+if [ "$#" -lt 2 ]; then echo "Usage: $0 NVENC|S2TC <input>"; exit 1; fi
+method="$1"
+shift
+filename="$1"
+pngfilename=`basename -s .svg "$filename"`".png"
+case $method in
+NVENC)
 inkscape --export-type="png" "$filename"
-if [ "$1" == "NVENC" ]; then
-	NVENC "$pngfilename"
-	exit 0
-elif [ "$1" == "S2TC" ]; then
-	S2TC "$pngfilename"
-	exit 0
-else
-	rm "$pngfilename"
-	exit 0
-fi
+NVENC "$pngfilename"
+;;
+S2TC)
+inkscape --export-type="png" "$filename"
+S2TC "$pngfilename"
+;;
+*)
+echo 'Only NVENC or S2TC is supported'
+;;
+esac
