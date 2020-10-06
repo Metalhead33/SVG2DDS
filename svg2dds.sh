53c8242ef3beed8e2bf8585253fa27b0e2d3c3c3
Zsolt Tóth <waysofdarkness@gmail.com>
2020-10-06 15:27:05 +0200

    Added demand for output

--- svg2dds.sh
+++ svg2dds.sh
@@ -7,7 +7,7 @@ die() {
 
 NVENC() {
 	name="$1"
-    nvcompress -bc1 "$name.png" "$name.dds"
+    nvcompress -bc1 "$name.png" "$2"
 	rm "$name.png"
 }
 
@@ -18,16 +18,18 @@ S2TC() {
 	export S2TC_DITHER_MODE=FLOYDSTEINBERG
 	name="$1"
 	convert "$name.png" "$name.tga"
-	s2tc_compress -i "$name.tga" -o "$name.dds" -t DXT1
+	s2tc_compress -i "$name.tga" -o "$2" -t DXT1
 	rm "$name.png"
 	rm "$name.tga"
 }
 
-[ $# -lt 2 ] && die "Usage: $(basename "$0") NVENC|S2TC <input.svg>"
+[ $# -lt 3 ] && die "Usage: $(basename "$0") NVENC|S2TC <input.svg> <output.dds>"
 
 method="$1"
 shift
 filename="$1"
+shift
+output="$1"
 shift
 base="$(basename -s .svg "$filename")"
 
